bba45860f8b5d3957da82bac6f69458e8734f670
joqtan <joqtanmarmol@gmail.com>
2021-04-14 16:28:03 -0600

    Sidebar and no content style

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,7 +1,14 @@
 import React from 'react'
+// import NothingSelected from './NothingSelected'
+import Sidebar from './Sidebar'
 
 const JournalScreen = () => {
-    return <div>Journal Screen</div>
+    return (
+        <div className='journal__main-content'>
+            <Sidebar />
+            <main>{/* <NothingSelected /> */}</main>
+        </div>
+    )
 }
 
 export default JournalScreen
